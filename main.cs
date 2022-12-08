0cb820beb353358991e30c640f7ee35df8a4d2a4
Aline F. Brito <alinefbrito@gmail.com>
2022-12-08 23:24:00 +0000

    Incluída explicação de parâmetros  E/OU em condicional e if concatenado

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ class Program {
   public static void Main (string[] args) {
     //declara uma variavel do tipo Inteiro e uma do tipo caractere(texto)
     //A variável String recebe o valor digitado e armazena. Toda entrada é sempre um texto
-    Int16 num;
+  Int16 num;
     String entrada;
     //Escreval
     Console.WriteLine("Digite um nro");
@@ -14,12 +14,30 @@ class Program {
     num = Int16.Parse(entrada);
 //se(num%2=0) entao 
     //C# a comparação é feita com ==
-    if(num%2==0)
+
+    
+    if (num%2==0)
+    {Console.WriteLine("Nro par");
+     //condição Se dentro de outra condição
+     if (num%3==0)
+     {
+       Console.WriteLine("Nro divisivel por 3");
+     }
+     
+     }
+    else
     {
-      Console.WriteLine("Número Par");
+      Console.WriteLine("Nro Impar");
+    }
+
+  //if com condiçoes E --> &&
+    // OU --> ||
+    if(num%3 ==0  && num%2==0)
+    {
+      Console.WriteLine("Nro divisivel por 2 e 3 ao mesmo tempo");
     }
     else{
-      Console.WriteLine("Nro Impar");
+      Console.WriteLine("Nro não divisivel por 2 e 3 ao mesmo tempo");
     }
     
     
